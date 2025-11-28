df304d622771e0c36de0f0e0c0fa1b128d7f0181
Alvaro Gonzalez <alvaro20dam@hotmail.com>
2025-11-27 20:18:43 -0400

    Finished Gatsby tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,20 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: "Web Portfolio",
+    description: "Site where I show my portfolio projects",
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
